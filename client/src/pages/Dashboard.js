3fbadeedde440f9a5de44ece159e3d01f1cb0dd4
mcasa1 <martincasaa@gmail.com>
2022-08-28 16:26:17 -0300

    update

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,6 +8,8 @@ import { useNavigate } from 'react-router-dom'
 
 
 const Dashboard = () => {
+    window.alert("Hola, gracias por estar usando la app. Como esto es una herramienta gratuita, esta construida sobre servidores gratuitos que tienen ciertas limitaciones. Es por eso que para que la app te pueda funcionar bien, es necesario que no cargues más de 200 figuritas entre las que necesitas y tus repetidas. Espero que igualmente asi te sirva")
+
     const [user, setUser] = useState(null)
     const cookies = useCookies(['user'])
     
@@ -66,6 +68,7 @@ const Dashboard = () => {
                 
            
             </div>}
+            
         
         
         </div>
